2d404f4a03f585a064260acaa2191c8d9f236978
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-05-24 19:41:24 -0300

    first commit

---
+++ src/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function Home () : JSX.Element {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(): JSX.Element {
+  return (
+      <header>
+        <span>DragonJS</span>
+      </header>
+  )
+}
+
+export default Header
